3f37f2bc58748f39101848ecc26f05c5155e94fd
ChrisV001 <kvasilovski@yahoo.com>
2025-04-04 00:03:15 +0200

    Footer Insertion

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span>
+                    
+                </span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
         <Navbar />
       </div>
       <h1>Hello World</h1>
+      <Footer />
     </div>
   );
 };
