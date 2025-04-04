781d90a0cd7b4c02522b8a7fcb52955f0db2449e
ChrisV001 <kvasilovski@yahoo.com>
2025-04-04 02:16:47 +0200

    Added axios, return section for Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,40 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/articles/all")
+      .then((response) => {
+        if (response.status.statusCode === 200) {
+          setArticles(response.data.articleList);
+        } else {
+          setError(response.data.message);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  });
   return (
     <div>
-      <div>
-        <Navbar />
+      <Navbar />
+      <div className="container mx-auto p-4 mt-20">
+        <h1 className="text-3xl font-bold mb-4">Articles</h1>
+        {error && <p className="text-red-600">{error}</p>}
+        {articles.length === 0 ? (
+          <p>No articles found</p>
+        ) : (
+          <ul>
+            
+          </ul>
+        )}
       </div>
-      <h1>Hello World</h1>
       <Footer />
     </div>
   );
