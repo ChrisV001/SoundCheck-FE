6c6260230d0970b3bb50319f02da1d3f4aad244a
ChrisV001 <kvasilovski@yahoo.com>
2025-04-05 03:57:56 +0200

    Added registration page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css'
-import Navbar from './components/Navbar';
-import Login from './pages/Login'
-import Home from './pages/Home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login onLoginSuccess={(data) => console.log(data)} />} />
+        <Route
+          path="/login"
+          element={<Login onLoginSuccess={(data) => console.log(data)} />}
+        />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
