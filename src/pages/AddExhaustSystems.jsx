7aef4246248f818b12c4d5090e857097545b6453
ChrisV001 <kvasilovski@yahoo.com>
2025-07-02 18:17:36 +0200

    Small fixes.

--- src/pages/AddExhaustSystems.jsx
+++ src/pages/AddExhaustSystems.jsx
@@ -21,7 +21,7 @@ const AddExhaustSystems = () => {
       .get("http://localhost:8080/carmodel/all")
       .then((response) => {
         if (response.data.statusCode === 200) {
-          setAllCarModels(response.data.carModels || []);
+          setAllCarModels(response.data.carModelList || []);
         } else {
           setError("Could not load car models: " + response.data.message);
         }
@@ -141,15 +141,15 @@ const AddExhaustSystems = () => {
                 </div>
                 <div>
                   <label
-                    htmlFor="soundprofile"
+                    htmlFor="soundProfile"
                     className="mr-4 block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Sound Profile
                   </label>
                   <input
-                    type="soundprofile"
-                    id="soundprofile"
-                    name="soundprofile"
+                    type="soundProfile"
+                    id="soundProfile"
+                    name="soundProfile"
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Sound Profile"
                     required={true}
